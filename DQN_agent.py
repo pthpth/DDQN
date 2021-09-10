366045644be94623ef1441d4f8e8dcf6446f4f72
DarkLord0206 <tulsyan.parth0206@gmail.com>
2021-09-10 15:18:57 +0530

    Final

--- DQN_agent.py
+++ DQN_agent.py
@@ -1,103 +1,127 @@
 import numpy as np
-from tensorflow.keras.layers import Dense
+
+import gym
+import highway_env
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
-from gym import Gym
-import pyglet
-from opengltest import GamerTab
+from keras.regularizers import l1, l2
 
 
-class DQNAgent:
+class DDQNAgent:
     def __init__(self, state_size, action_size):
         self.n_actions = action_size
         self.lr = 0.005
-        self.gamma = 0.99
-        self.exploration_proba = 0.8
-        self.exploration_proba_decay = 0.005
-        self.batch_size = 4
+        self.gamma = 0.75
+        self.exp_proba = 0.8
+        self.exp_proba_decay = 0.000
         self.memory_buffer = list()
         self.max_memory_buffer = 2000
-        self.model = Sequential([
-            Dense(units=24, input_dim=state_size, activation='relu'),
+        self.q_model = self.build_model(state_size, action_size)
+        self.q_target_model = self.build_model(state_size, action_size)
+
+    def build_model(self, state_size, action_size):
+        model = Sequential([
+            Dense(units=100, input_dim=state_size, activation='relu'),
+            Dense(units=50, activation='relu', bias_regularizer=l2(0.01), kernel_regularizer=l2(0.01)),
             Dense(units=24, activation='relu'),
             Dense(units=action_size, activation='linear')
         ])
-        self.model.compile(loss="mse",
-                           optimizer=Adam(lr=self.lr))
+        # model=keras.models.load_model('model')
+        model.compile(loss="mse", optimizer=Adam(lr=self.lr))
+        return model
 
     def compute_action(self, current_state):
-        if np.random.uniform(0, 1) < self.exploration_proba:
+        if np.random.uniform(0, 1) < self.exp_proba:
             return np.random.choice(range(self.n_actions))
-        q_values = self.model.predict(current_state)
-        print("q_values", q_values)
-        return np.argmax(q_values[0])
-
-    def update_exploration_probability(self):
-        self.exploration_proba = self.exploration_proba * np.exp(-self.exploration_proba_decay)
+        q_values = self.q_model.predict(current_state)[0]
+        return np.argmax(q_values)
 
     def store_episode(self, current_state, action, reward, next_state, done):
         self.memory_buffer.append({
-            "current_state": np.asarray(current_state).astype(np.float32),
+            "current_state": current_state,
             "action": action,
             "reward": reward,
-            "next_state": np.asarray(next_state).astype(np.float32),
+            "next_state": next_state,
             "done": done
         })
         if len(self.memory_buffer) > self.max_memory_buffer:
             self.memory_buffer.pop(0)
 
-    def train(self):
+    def update_exploration_probability(self):
+        self.exp_proba = self.exp_proba * np.exp(-self.exp_proba_decay)
+        print(self.exp_proba)
+
+    def train(self, batch_size):
         np.random.shuffle(self.memory_buffer)
-        batch_sample = self.memory_buffer[0:self.batch_size]
+        batch_sample = self.memory_buffer[0:batch_size]
+
         for experience in batch_sample:
-            q_current_state = self.model.predict(experience["current_state"])
+            q_current_state = self.q_model.predict(experience["current_state"])
             q_target = experience["reward"]
             if not experience["done"]:
-                print("error", experience["next_state"].shape)
-                q_target = q_target + self.gamma * np.max(self.model.predict(experience["next_state"])[0])
+                q_target = q_target + self.gamma * np.max(self.q_target_model.predict(experience["next_state"])[0])
             q_current_state[0][experience["action"]] = q_target
-            self.model.fit(experience["current_state"], q_current_state, verbose=0)
+            self.q_model.fit(experience["current_state"], q_current_state, verbose=0)
+
+    def update_q_target_network(self):
+        self.q_target_model.set_weights(self.q_model.get_weights())
 
 
-env = Gym()
+config = {
+    "observation": {
+        "type": "Kinematics",
+        "vehicles_count": 50,
+        "features": ["presence", "x", "y", "vx", "vy", "cos_h", "sin_h"],
+        "features_range": {
+            "x": [-200, 200],
+            "y": [-200, 200],
+            "vx": [-10, 10],
+            "vy": [-10, 10]
+        },
+        "normalize": True,
+        "absolute": False,
+        "order": "sorted"
+    }
+}
+env = gym.make('highway-v0')
+env.configure(config)
 
-state_size = 6
-action_size = 4
-
-n_episodes = 10000
-runner = 0
-
-agent = DQNAgent(state_size, action_size)
-
+state_size = 350
+action_size = 5
+n_episodes = 500
+max_iteration_ep = 1000
+batch_size = 8
+q_target_update_freq = 5
+agent = DDQNAgent(state_size, action_size)
 total_steps = 0
-
+n_training = 0
 for e in range(n_episodes):
+    current_state = env.reset()
+    current_state = np.reshape(current_state, (1, 350))
+    rewards = 0
+    if e > 350:
+        agent.exploration_proba = 0
+    for step in range(max_iteration_ep):
+        env.render()
+        total_steps = total_steps + 1
+        action = agent.compute_action(current_state)
+        next_state, reward, done, _ = env.step(action)
+        next_state = np.reshape(next_state, (1, 350))
+        rewards = rewards + reward
+        agent.store_episode(current_state, action, reward, next_state, done)
 
-    current_state = env.input_generator()
-    current_state = np.asarray([current_state]).astype(np.float32)
-    total_steps = total_steps + 1
-    runner += 1
-
-    action = agent.compute_action(current_state)
-
-    next_state, reward, done = env.step(action)
-
-    next_state = np.array([next_state])
-
-    agent.store_episode(current_state, action, reward, next_state, done)
-
-    if done:
-        env = Gym()
-        next_state = env.input_generator()
-        agent.update_exploration_probability()
-    current_state = next_state
-
-    if total_steps >= agent.batch_size:
-        total_steps = 0
-        agent.train()
-
-    if runner % 100 == 0:
-        runner = 0
-        tester = GamerTab(agent.model, 400, 400, "hello")
-        print("oi")
-        pyglet.app.run()
+        if done:
+            # agent.update_exploration_probability()
+            print(agent.exp_proba)
+            break
+        current_state = next_state
+    if e % 100 == 0:
+        agent.q_model.save("model1")
+    print("episode ", e + 1, " rewards: ", rewards)
+    if total_steps >= batch_size:
+        agent.train(batch_size=batch_size)
+        n_training = n_training + 1
+        if n_training % q_target_update_freq:
+            agent.update_q_target_network()
+agent.q_model.save("model1")
